628a2e6492a3d4e04561022f2c21490c6c0980f8
Michel Blancard <michel.blancard@data.gouv.fr>
2016-07-27 12:24:53 +0200

    JavaScript JSON Graph serialization/unserialization

--- src/json_graph.js
+++ src/json_graph.js
@@ -2,6 +2,167 @@
 
 import read from 'read-file-stdin'
 
+function is_ofnode(value) {
+  return ((value instanceof Object) && ('type' in value));
+}
+
+function of_equality(a, b) {
+  console.assert (is_ofnode(a));
+  console.assert (is_ofnode(b));
+
+  if (Object.keys(a).length != Object.keys(b).length) {
+    return false;
+  }
+
+  for (var k in a) {
+    if (!(k in b)) {
+      return false;
+    }
+
+    if (is_ofnode(a.k)) {
+      if (!is_ofnode(b.k)) {
+        return false;
+      }
+      if (!of_equality(a.k, b.k)) {
+        return false;
+      }
+    } else if (a.k instanceof Array) {
+      if (!(b.k instanceof Array)) {
+        return false;
+      }
+      if (a.k.length != b.k.length) {
+        return false;
+      }
+      for (var i = 0; i < a.k.length; i++) {
+        if (is_ofnode(a.k[i])) {
+          if (!(is_ofnode(b.k[i]))) {
+            return false;
+          }
+          if (!of_equality(a.k[i], b.k[i])) {
+            return false;
+          }
+        } else {
+          if (a.k[i] != b.k[i]) {
+            return false;
+          }
+        }
+      }
+    } else {
+      if (a.k !== b.k) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+
+function serialize(input_nodes) {
+  let visited_nodes = [];
+  let output_nodes = [];
+  let edges = [];
+
+  let append_edge = edge => {
+    /*
+    Mutates `edges` in outer scope.
+
+    Workaround due to a limitation of graphlib npm module which has only `setEdge` method.
+    Consequence: it is not possible to add many edges having the same source and target.
+    Even if graphviz supports it.
+    See: https://github.com/cpettitt/graphlib/blob/master/lib/graph.js#L353
+    */
+    for (var i = 0; i < edges.length; i++) {
+      let edge1 = edges[i];
+      if ((edge['source'] === edge1['source']) &&
+          (edge['target'] === edge1['target']))
+      {
+        edge1['label'] += '\n' + edge['label'];
+        return;
+      }
+    }
+    edges.push(edge);
+  }
+
+  let ShortIdGenerator = function* (object) {
+    let next_shortid = 0;
+
+    while (true) {
+      yield next_shortid++;
+    }
+  }
+
+  let short_id_generator = ShortIdGenerator();
+
+  let visit = ofnode => {
+    console.assert (is_ofnode(ofnode));
+
+    if ('_id' in ofnode) {
+      return;
+    }
+
+    let ofnode_id = short_id_generator.next().value
+    let jgfnode = Object.assign({}, ofnode);
+    jgfnode['id'] = ofnode_id;
+    ofnode['_id'] = ofnode_id;
+    visited_nodes.push(ofnode);
+
+    for (let key in ofnode) {
+      let value = ofnode[key];
+      if (value instanceof Array) {
+        if (key !== 'path') {
+          delete jgfnode[key];
+          for (var i = 0; i < value.length; i++) {
+            let item = value[i];
+            if (is_ofnode(item)) {
+              visit(item);
+              append_edge({
+                'source': ofnode_id,
+                'target': item._id,
+                'label': key + '[' + i + ']',
+                });
+            }
+          }
+        }
+      } else if (is_ofnode(value)) {
+        delete jgfnode[key];
+        visit(value);
+        let edge = {
+          'source': ofnode_id,
+          'target': value._id,
+          'label': key,
+          };
+        // if ofnode['type'] == 'ValueForPeriod' and key == 'period':
+        //     edge['color'] = 'red'
+        append_edge(edge);
+      }
+    }
+    output_nodes.push(jgfnode);
+  }
+
+  if (input_nodes instanceof Array) {
+    for (var i = 0; i < root_ofnode.length; i++) {
+      item = input_nodes[i];
+      visit(item);
+    }
+  } else {
+    visit(input_nodes);
+  }
+
+  // Remove _id keys in input objects
+  for (var i = 0; i < visited_nodes.length; i++) {
+    let visited_node = visited_nodes[i];
+    delete visited_node['_id'];
+  }
+
+  return {
+      'graph': {
+          'directed': true,
+          'nodes': output_nodes,
+          'edges': edges,
+          },
+      }
+}
+
 
 function unserialize(json_graph) {
   console.assert(Object.keys(json_graph).length == 1);
@@ -17,10 +178,12 @@ function unserialize(json_graph) {
 
   for (var i = 0; i < input_nodes.length; i++) {
     let input_node = input_nodes[i];
-    let input_id = input_node.id;
-    delete input_node['id'];
+    let node_id = input_node.id;
 
-    output_nodes[input_id] = input_node;
+    let output_node = Object.assign({}, input_node);
+    delete output_node['id'];
+
+    output_nodes[node_id] = output_node;
   }
 
   for (var i = 0; i < edges.length; i++) {
@@ -37,10 +200,10 @@ function unserialize(json_graph) {
       source_node[label] = target_node;
     } else {
       let regexp = /^operands\[([0-9]+)\]/;
-      let index = regexp.exec(label);
-      if (index) {
-        index = parseInt(index);
-        if (!('operands' in Object.keys(source_node))) {
+      let regexp_result = regexp.exec(label);
+      if (regexp_result) {
+        let index = parseInt(regexp_result[1]);
+        if (!('operands' in source_node)) {
           source_node.operands = [];
         }
         source_node.operands[index] = target_node;
@@ -53,15 +216,28 @@ function unserialize(json_graph) {
 }
 
 
-function main(json_graph) {
-  let nodes = unserialize(json_graph);
+function main(json_buffer) {
+  const json_graph1 = JSON.parse(json_buffer);
+  let json_string1 = JSON.stringify(json_graph1);
 
-  console.log(nodes);
+  let nodes1 = unserialize(json_graph1);
+  let root_node1 = nodes1[0];
+
+  let json_graph2 = serialize(root_node1);
+  let json_string2 = JSON.stringify(json_graph2);
+
+  let nodes2 = unserialize(json_graph2);
+  let root_node2 = nodes2[0];
+
+  //console.log(nodes);
+  console.log(json_string1);
+  console.log(json_string2);
+  console.log(of_equality(root_node1, root_node2));
 }
 
 
 read(process.argv[2], (err, buffer) => {
   if (err) { throw err; }
-  const json_graph = JSON.parse(buffer);
-  main(json_graph);
+
+  main(buffer);
 });
